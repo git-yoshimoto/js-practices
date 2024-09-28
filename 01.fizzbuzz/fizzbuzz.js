d89dbfeba53b2c4996093b62f4141ec6adfa506b
yoshimoto <work.s.yoshimoto@gmail.com>
2024-09-28 23:44:34 +0900

    if文をブロックを明示化する修正を実施

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,6 +1,10 @@
 for (let i = 1; i <= 20; i++ ){
   let output = '';
-  if (i % 3 === 0) output += 'Fizz';
-  if (i % 5 === 0) output += 'Buzz';
+  if (i % 3 === 0) {
+    output += 'Fizz';
+  }
+  if (i % 5 === 0) {
+    output += 'Buzz';
+  }
   console.log(output || i);
 }
